1195df4bac8c47b8f928d4ba5019324b117d9d70
User <user@example.com>
2025-12-03 00:28:02 +0800

    update baseurl

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/japanese-vocab/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/'
   server: {
     port: 5173
   }
